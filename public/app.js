8f3d45c2e3a09393649ec43701dd7b8d1f803c97
ayoub-atif <ayoubatif449@gmail.com>
2023-06-09 15:19:33 +0100

    exo3

--- public/app.js
+++ public/app.js
@@ -35,18 +35,19 @@ btn_list.addEventListener("click",(param)=>{
             close[i].parentNode.style.display = "none";
         })
     }
+    // *we need empty input so i did this:
     input.value = "";
     
    
 });
-
+// *if u click the color of the botton goona change to green:
 document.addEventListener("click", param =>{
     if (param.target.className.includes("check")) {
         param.target.parentElement.parentElement.classList.toggle("green");
     }
 })
 
-
+//?if u click the botton goona change to blue:
 document.addEventListener("click", param =>{
     if (param.target.className.includes("update")) {
         param.target.parentElement.parentElement.classList.toggle("blue");
@@ -54,4 +55,5 @@ document.addEventListener("click", param =>{
         param.target.parentElement.previousElementSibling.textContent = entrer;
     }
 })
+// ******************************
 
